d2d011eb526b8b2c81d79c747ca59774c6891e59
dk <dk90@mail.ru>
2019-11-24 00:27:21 +0200

    Add comments

--- watcher.sh
+++ watcher.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-etokenID=$(lsusb -d 0529:0600); # Testing single Alading eToken ID
-loopTimer=$1;
+# Optional 'help' and 'processFinder' parameters are available.
+# The script should be runnable as a daemon with empty parameters. 
+
+etokenID=$(lsusb -d 0529:0600) # Testing single Alading eToken ID
+loopTimer=3
 
 help() {
-
 	
 }
 
 processFinder() {
-
-	
+	local pnames=$(lsof /usr/lib/libeToken.so | cut -d' ' -f1)
+	echo "$pnames"
 }
 
 main() {
-
 	
 }
