a89bcef9f99bcaf161c78a27430de1fa3991f0c2
dk <dk90@mail.ru>
2019-11-24 18:38:46 +0200

    Rename function, etc

--- watcher.sh
+++ watcher.sh
@@ -15,6 +15,7 @@ help() {
 	echo "-s  --showp	show processes use eToken"
 	echo "-n, --nolock	suppress DE locker and don't lock current session"
 	echo
+	return 0
 }
 
 pFinder() {
@@ -22,6 +23,7 @@ pFinder() {
 		$LSOF /usr/lib/libeToken.so | cut -d' ' -f 1-5
 	else
 		echo "There is no lsof command or 'libeToken.so' file has been found"
+		return 126
 	fi
 }
 
@@ -34,44 +36,46 @@ pKiller() {
 	# Veracrypt provides unmount volume possibilities not being as root,
 	# just when the volumes mounted as read-only.
 	# You need to be in the appropriate group to unmount all volumes.
-	veracrypt -d; echo "Veracrypt users' containers have been unmounted"
+	veracrypt -d && echo "Veracrypt users' containers have been unmounted"
+	return 0
 }
 
 lockFinder() {
 	echo "Nothing yet" # Detect DE's locker command. Xfce4 light-locker only supported.
+	return 0
 }
 
-eSpy() {
+eAgent() {
 	case "$1" in
 		-n | --nolock)
 			while : ; do
-				sleep $LOOPTIMER
+				local timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 				local etokenID=$(lsusb -d 0529:0600) # Testing single Alading eToken ID. Any card or token should be detected automatically here.
+				sleep $LOOPTIMER
 				if [ -n "$etokenID" ]; then
 					echo "eToken is online"
 					continue
 				else
 					# Need to have eToken online every time because it kills
 					# every processes not being used the eToken either.
-					pKiller
-					echo "eToken related processes have been killed"
+					pKiller && echo "eToken related processes have been killed - $timestamp"
 				fi
 			done
 		;;
 		*)
 			while : ; do
-				sleep $LOOPTIMER
+				local timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 				local etokenID=$(lsusb -d 0529:0600) # Testing single Alading eToken ID. Any card or token should be detected automatically here.
+				sleep $LOOPTIMER
 				if [ -n "$etokenID" ]; then
 					echo "eToken is online"
 					continue
 				else
 					# Need to have eToken online every time because it kills
 					# every processes not being used the eToken either.
-					pKiller
+					pKiller && echo "eToken related processes have been killed - $timestamp"
 					# xflock4 checker should be here in order to get rid
 					# doing it every time.
-					echo "eToken related processes have been killed and locked"
 				fi
 			done
 		;;
@@ -84,14 +88,14 @@ case "$1" in
 	;;
 	-n | --nolock)
 		# Some pretests here
-		eSpy --nolock
+		eAgent --nolock
 	;;
 	-s | --showp)
 		pFinder
 	;;
 	*)
 		# Some pretests here
-		eSpy
+		eAgent
 	;;	
 esac
 
