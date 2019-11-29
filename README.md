4166dff2946bb32a930f3104e015e807d0d3ceac
dk <dk90@mail.ru>
2019-11-29 23:51:53 +0200

    Code review, functionality logic, additional comments and more plans

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Background bash process is watching for eToken USB serial status and makes decision for killing all processes that were authorized by the eToken or any smart-card device.
 
-The script covers for terminating the next processes: VeraCrypt crypted volumes and unmounting, OpenVPN and SSH disconnecting sessions, screenlocker xfce4 caller.
+The script covers for terminating the next processes: Unmount VeraCrypt crypted volumes, OpenVPN and SSH disconnecting sessions, screenlocker caller.
--- ewatcher.sh
+++ ewatcher.sh
@@ -10,13 +10,14 @@
 # Email			:	haxprox@gmail.com
 #############################################################################################################################################################################
 
-# . - Most probably need to load some configs when systemd daemon will be reviewed.
-# Check one more function that just locking and do nothing anymore.
+# . - Most probably need to load some configs when systemd daemon will be reviewed. {1}
+# rpm and deb packeg is needed {2}
 
 declare -ri LOOPTIMER=5
 declare -r LSOF=/usr/bin/lsof
 
 help() {
+	
 	echo "Usage: $0 [option...]"
 	echo
 	echo "-h, --help	show this dialog"
@@ -24,14 +25,16 @@ help() {
 	echo "-n, --nolock	suppress DE locker and don't lock current session"
 	echo "-l, --lock	Just call DE locker and nothing more"
 	echo "-k, --knlock	Kill everyhing depends to the eToken and lock"
+	echo "-o, --logout	Kill everyhing depends to the eToken and logout"
 	echo
 	exit 0
 }
 
 pFinder() {
+	
 	case "$1" in
 		-s | --showp)
-			if [ -x $LSOF ] && [ -e /usr/lib/libeToken.so ]; then
+			if [[ -x $LSOF ]] && [[ -e /usr/lib/libeToken.so ]]; then
 				$LSOF /usr/lib/libeToken.so | cut -d' ' -f 1-5
 			else
 				echo "There is no lsof command or 'libeToken.so' file has been found"
@@ -39,7 +42,7 @@ pFinder() {
 			fi
 		;;
 		-f | --find)
-			if [ -x $LSOF ] && [ -e /usr/lib/libeToken.so ]; then
+			if [[ -x $LSOF ]] && [[ -e /usr/lib/libeToken.so ]]; then
 				$LSOF -t /usr/lib/libeToken.so # OpenSC should be added as well.
 			else
 				echo "There is no lsof command or 'libeToken.so' file has been found"
@@ -53,7 +56,12 @@ pFinder() {
 	esac
 }
 
+eSaveSession { # Execute save session command for each DE only once before 'eAgent' lopping. 
+	# Pre-test here at first in order to make sure all commands are available
+}
+
 pKiller() { # Process killer
+	
 	local i
 	for i in $(pFinder -f); do
 		if kill $i; then
@@ -71,61 +79,89 @@ pKiller() { # Process killer
 	return 0
 }
 
-eScreenLocker() {
+eScreenLocker() { # Locker and logout
 	
-	case "$XDG_CURRENT_DESKTOP" in
-		XFCE)
-			# Some checker here then locking
-			#xflock4
-		;;
-		KDE)
-			# Some checker here then locking
-			# loginctl "lock-session" or something like similar: "qdbus org.kde.ksmserver /ScreenSaver org.freedesktop.ScreenSaver.Lock"
-		;;
-		GNOME)
-			# Some checker here then locking
-			# bus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock
-		;;
-		MATE)
-			# Some checker here then locking
-			# mate-screensaver-command -l
-		;;
-		CINNAMON)
-			# Some checker here then locking
-			# bash -c "cinnamon-screensaver-command -l; xset dpms force off;"
-			# or "cinnamon-screensaver-command -l"
-		;;			
-	esac
+	if [[ -n $XDG_CURRENT_DESKTOP ]]; then
+		case "$XDG_CURRENT_DESKTOP" in
+			XFCE)
+				if [[ "$1" == "--logout" ]]; then
+					# Some checker here then locking
+					# Logout command
+				else
+					xflock4
+				fi
+			;;
+			KDE)
+				if [[ "$1" == "--logout" ]]; then
+					# Some checker here then locking
+					# Logout command
+				else
+					#loginctl "lock-session" or something like: "qdbus org.kde.ksmserver /ScreenSaver org.freedesktop.ScreenSaver.Lock"
+				fi
+			;;
+			GNOME)
+				if [[ "$1" == "--logout" ]]; then
+					# Some checker here then locking
+					# Logout command
+				else
+					bus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock
+				fi
+			;;
+			MATE)
+				if [[ "$1" == "--logout" ]]; then
+					# Some checker here then locking
+					# Logout command
+				else
+					mate-screensaver-command -l
+				fi
+			;;
+			CINNAMON)
+				if [[ "$1" == "--logout" ]]; then
+					# Some checker here then locking
+					# Logout command
+				else
+					cinnamon-screensaver-command -l
+				fi
+			;;			
+		esac
+	else
+		echo -e "There is no \e[102m"$XDG_CURRENT_DESKTOP"\e[0m variable has been found. Unable to detect current X session"
+		exit 0 
+	fi
 	return 0
 }
 
-eAgent() {	
+eAgent() {
+	
 	while : ; do
 		local timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 		local etokenID=$(lsusb -d 0529:0600)
 		# Testing single Alading eToken ID,
 		# any card or token should be detected automatically here.
-		if [ -n "$etokenID" ]; then
-			clear
-			echo -e "eToken $etokenID is \e[102monline\e[0m now - $timestamp" # Spinner here?
+		if [[ -n $etokenID ]]; then
+			clear; echo -e "eToken $etokenID is \e[102monline\e[0m now - $timestamp" # Spinner here?
 			sleep $LOOPTIMER
 			continue
 		else
 			case "$1" in
 				-n | --nolock)
-					if [ $(pFinder -f) ]; then
+					if [[ $(pFinder -f) ]]; then
 						pKiller && echo -e "eToken related processes have been killed - \e[102m$timestamp\e[0m"
 					fi
 				;;
 				-l | --lock)
-					if [ 1 ]; then # One statement condition in order to execute xflock4(locker) once.
-						eScreenLocker && echo -e "eToken is out now. The system has been locked - \e[102m$timestamp\e[0m"
-					fi
+				# One statement condition in order to execute locker once.
+				# Unable to control this SHIT!!111 but it make sense to have session $LOOPTIMER locked or
+				# locked all the time
+						clear; eScreenLocker && echo -e "eToken is out now. The system has been locked - \e[102m$timestamp\e[0m"
 				;;
 				-k | --knlock)
-					if [ $(pFinder -f) ]; then
-						pKiller && eScreenLocker && echo -e "eToken related processes have been killed and locked - \e[102m$timestamp\e[0m"
+					if [[ $(pFinder -f) ]]; then
+						pKiller; eScreenLocker && echo -e "eToken related processes have been killed and locked - \e[102m$timestamp\e[0m"
 					fi
+				-o | --logout)
+						# "eScreenLocker --logout" method for logout. Save session or without one?
+						pKiller; eScreenLocker --logout && echo -e "eToken related processes have been killed and logout - \e[102m$timestamp\e[0m"
 				;;
 				*)
 					echo "\e[102mInvalid option\e[0m"
@@ -135,8 +171,7 @@ eAgent() {
 		fi
 		sleep $LOOPTIMER
 	done
-
-return 0
+	return 0
 }
 
 case "$1" in
@@ -154,6 +189,10 @@ case "$1" in
 	-k | --knlock)
 		# Some pretests here
 		eAgent --knlock
+	-o | --logout)
+		# Some pretests here
+		eSaveSession
+		eAgent --logout
 	;;
 	-s | --showp)
 		pFinder --showp
